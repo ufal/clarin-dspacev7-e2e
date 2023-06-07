8f97f986fe53f171a8dd53a6e208032720e04cb8
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-07 17:36:19 +0200

    Privacy statement should not be lorem ipsum

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     "experimentalStudio": true,
     viewportHeight: 1080,
     viewportWidth: 1920,
+    baseUrl: "https://ufal-point-dev.ufal.hide.ms.mff.cuni.cz",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,12 @@ describe('ufal-point-dev', () => {
   /* ==== Test Created with Cypress Studio ==== */
   it('loads', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('https://ufal-point-dev.ufal.hide.ms.mff.cuni.cz');
+    cy.visit('/');
     /* ==== End Cypress Studio ==== */
+  });
+
+  it('privacy statement should not be lorem ipsum', function() {
+    cy.visit('/info/privacy');
+    cy.contains('Lorem ipsum dolor sit amet', {timeout: 100}).should('not.exist');
   });
 })
