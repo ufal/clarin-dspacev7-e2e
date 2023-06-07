ff8557f5132429967306615785839bc044e93cae
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-07 17:22:24 +0200

    ufal-point-dev loads in a normal size viewport

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "experimentalStudio": true,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('ufal-point-dev', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('loads', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://ufal-point-dev.ufal.hide.ms.mff.cuni.cz');
+    /* ==== End Cypress Studio ==== */
+  });
+})
