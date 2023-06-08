7885d9ddb1505b140fc5fd4833be454e1390447f
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-08 15:09:23 +0200

    lang iso_code should be name in facets

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,4 @@
+const noRetry = {timeout: 0};
 describe('ufal-point-dev', () => {
   /* ==== Test Created with Cypress Studio ==== */
   it('loads', function() {
@@ -8,6 +9,16 @@ describe('ufal-point-dev', () => {
 
   it('privacy statement should not be lorem ipsum', function() {
     cy.visit('/info/privacy');
-    cy.contains('Lorem ipsum dolor sit amet', {timeout: 100}).should('not.exist');
+    cy.contains('Lorem ipsum dolor sit amet', noRetry).should('not.exist');
+  });
+
+  it('Language names instead of iso', function() {
+    cy.visit('/');
+    cy.contains('Language (ISO)').then(($el) => {
+      const p = cy.get($el.parent());
+      p.contains('ces (', noRetry).should('not.exist');
+      p.contains('eng (', noRetry).should('not.exist');
+      p.contains('zxx (', noRetry).should('not.exist');
+    })
   });
 })
