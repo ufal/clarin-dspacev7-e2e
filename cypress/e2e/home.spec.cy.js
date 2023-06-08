0619ebfa6fbac0d479639053e5c41d800eb4587b
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-08 21:28:23 +0200

    Separate into multiple files
    new test - should not use handles for navigation

---
+++ cypress/e2e/home.spec.cy.js
@@ -0,0 +1,29 @@
+import {noRetry} from "../support/e2e";
+
+describe('homepage', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('Language names instead of iso', function() {
+        cy.contains('Language (ISO)').then(($el) => {
+            const p = cy.get($el.parent());
+            p.contains('ces (', noRetry).should('not.exist');
+            p.contains('eng (', noRetry).should('not.exist');
+            p.contains('zxx (', noRetry).should('not.exist');
+        })
+    });
+
+    it('Should not use handles when navigating the repository', function() {
+        /**
+         * When using handles:
+         * 1. It's hard to test in non production environment (item-view opens in production)
+         * 2. It's an extra redirect repository -> handle server -> repository
+         * 3. You'd be goint from https (repository) to http (handle server) and back to https (repository)
+         */
+        cy.get('a.item-name').should('have.attr', 'href').then( (href) => {
+            expect(href).to.not.contain('hdl.handle.net');
+        });
+    });
+
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,24 +1,10 @@
-const noRetry = {timeout: 0};
+import {noRetry} from "../support/e2e";
+
 describe('ufal-point-dev', () => {
-  /* ==== Test Created with Cypress Studio ==== */
-  it('loads', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('/');
-    /* ==== End Cypress Studio ==== */
-  });
 
   it('privacy statement should not be lorem ipsum', function() {
     cy.visit('/info/privacy');
     cy.contains('Lorem ipsum dolor sit amet', noRetry).should('not.exist');
   });
 
-  it('Language names instead of iso', function() {
-    cy.visit('/');
-    cy.contains('Language (ISO)').then(($el) => {
-      const p = cy.get($el.parent());
-      p.contains('ces (', noRetry).should('not.exist');
-      p.contains('eng (', noRetry).should('not.exist');
-      p.contains('zxx (', noRetry).should('not.exist');
-    })
-  });
 })
