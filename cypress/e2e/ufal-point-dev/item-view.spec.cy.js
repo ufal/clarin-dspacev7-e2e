b850b249020f5cc6ac2aa763d1af1ca26a3faa0d
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-09 14:28:30 +0200

    check item view displays language names and not codes

--- cypress/e2e/ufal-point-dev/item-view.spec.cy.js
+++ cypress/e2e/ufal-point-dev/item-view.spec.cy.js
@@ -1,5 +1,15 @@
+import {noRetry} from "../../support/e2e";
+
 describe('item-view', () => {
-  it('corefud', function() {
+  it('corefud language names instead of iso', function() {
     cy.view_item();
+    cy.contains('Language(s)').then(($el) => {
+      cy.get($el.parent().parent().parent()).then(($lang_row) => {
+        cy.get($lang_row).contains('ces', noRetry).should('not.exist');
+        cy.get($lang_row).contains('eng', noRetry).should('not.exist');
+        cy.get($lang_row).contains('fra', noRetry).should('not.exist');
+      });
+      //debugger;
+    })
   });
 });
